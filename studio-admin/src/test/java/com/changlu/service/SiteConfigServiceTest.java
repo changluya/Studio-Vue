588257877466e6ea3106df8da0c17ab5a399ac45
939974883@qq.com <edy@192.168.10.108>
2024-07-30 01:08:06 +0800

    [feat_1.2.x_fsstudio][studio-admin][补充网站配置单测]

--- studio-admin/src/test/java/com/changlu/service/SiteConfigServiceTest.java
+++ studio-admin/src/test/java/com/changlu/service/SiteConfigServiceTest.java
@@ -38,43 +38,107 @@ public class SiteConfigServiceTest {
         System.out.println(basicConfig);
     }
 
+    //网站基础配置:site.basicConfig
     @Test
-    public void addOrUpdateSiteConfigTest() {
-        System.out.println("===========新增配置===============");
-        //新增配置
-        ConfigVo configVo = new ConfigVo();
-        configVo.setConfigKey(ConfigTypeEnum.SITE_BASICCONFIG.getConfigKey());
-        String json = "{\n" +
-                "        siteTitle: '仿生实验室',  \n" +
-                "        teamTitle: '仿生实验室',  \n" +
+    public void addOrUpdateSite_basicConfig_Test() {
+        String createJson = "{\n" +
+                "        siteTitle: '仿生实验室1',  \n" +
+                "        teamTitle: '仿生实验室1',  \n" +
                 "        teamLogo: 'http://pictured-bedtest.oss-cn-beijing.aliyuncs.com/test/studio/8c44b79e-cd18-40ae-8d5a-5e2a0df303b7.png',\n" +
                 "        ISPN: '京公网安备11000002000001号', \n" +
                 "        siteCreateTime: '2016'\n" +
                 "      }";
-        configVo.setConfigValue(json);
+        String updateJson = "{\n" +
+                "        siteTitle: '仿生实验室',  \n" +
+                "        teamTitle: '仿生实验室',  \n" +
+                "        teamLogo: 'http://pictured-bedtest.oss-cn-beijing.aliyuncs.com/test/studio/8c44b79e-cd18-40ae-8d5a-5e2a0df303b7.png',\n" +
+                "        ISPN: '京公网安备11000002000001号', \n" +
+                "        siteCreateTime: '2017'\n" +
+                "      }";
+        String configKey = ConfigTypeEnum.SITE_BASICCONFIG.getConfigKey();
+        addOrUpdateSiteConfigTest(createJson, updateJson, configKey);
+    }
+
+    //网站主页:site.page.mainConfig
+    @Test
+    public void addOrUpdateSite_page_mainConfig_Test() {
+        String createJson = "{\n" +
+                "        teamDescription: '这里是物联网工作室，在这里不仅有学习硬件，还有学习软件的小伙伴们，我们都在前进的路上，未来值得期待！\\n' +\n" +
+                "          '\\n' +\n" +
+                "          '工作室有着丰富的学习资源，有着可以帮助你解决问题的学长学姐们以及专业指导老师，让你不断在专业领域进行探索和挖掘知识宝藏。',\n" +
+                "        bannerTableData: [{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/071.png',\n" +
+                "          mainTitle: '欢迎来到仿生实验室1',\n" +
+                "          subTitle: '一群志同道合的人，一起奔跑在理想的路上...'\n" +
+                "        },{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/072.png',\n" +
+                "          mainTitle: '关于我们1',\n" +
+                "          subTitle: '最好的团队，最好的我们，不负韶华，努力奋斗。'\n" +
+                "        },{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/073.png',\n" +
+                "          mainTitle: '时光轴1',\n" +
+                "          subTitle: '时间是温柔的羽毛，把过往的灰尘轻轻弹去。'\n" +
+                "        },{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/074.png',\n" +
+                "          mainTitle: '团队1',\n" +
+                "          subTitle: '拍照只需要三秒，可锁住的是我们三年青春，感谢遇见！'\n" +
+                "        }]\n" +
+                "      }";
+        String updateJson = "{\n" +
+                "        teamDescription: '这里是物联网工作室，在这里不仅有学习硬件，还有学习软件的小伙伴们，我们都在前进的路上，未来值得期待！\\n' +\n" +
+                "          '\\n' +\n" +
+                "          '工作室有着丰富的学习资源，有着可以帮助你解决问题的学长学姐们以及专业指导老师，让你不断在专业领域进行探索和挖掘知识宝藏。',\n" +
+                "        bannerTableData: [{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/071.png',\n" +
+                "          mainTitle: '欢迎来到仿生实验室',\n" +
+                "          subTitle: '一群志同道合的人，一起奔跑在理想的路上...'\n" +
+                "        },{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/072.png',\n" +
+                "          mainTitle: '关于我们',\n" +
+                "          subTitle: '最好的团队，最好的我们，不负韶华，努力奋斗。'\n" +
+                "        },{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/073.png',\n" +
+                "          mainTitle: '时光轴',\n" +
+                "          subTitle: '时间是温柔的羽毛，把过往的灰尘轻轻弹去。'\n" +
+                "        },{\n" +
+                "          bannerImg: 'https://pictured-bed.oss-cn-beijing.aliyuncs.com/img/2024/074.png',\n" +
+                "          mainTitle: '团队',\n" +
+                "          subTitle: '拍照只需要三秒，可锁住的是我们三年青春，感谢遇见！'\n" +
+                "        }]\n" +
+                "      }";
+        String configKey = ConfigTypeEnum.SITE_PAGE_MAINCONFIG.getConfigKey();
+        addOrUpdateSiteConfigTest(createJson, updateJson, configKey);
+    }
+
+
+
+    public void addOrUpdateSiteConfigTest(String createJson, String updateJson, String configKey) {
+        System.out.println("===========1、新增配置===============");
+        //新增配置
+        ConfigVo configVo = new ConfigVo();
+        configVo.setConfigKey(configKey);
+        configVo.setConfigValue(createJson);
         System.out.println(siteConfigService.addOrUpdateSiteConfig(configVo));
-        System.out.println("===========查询配置===============");
+        System.out.println("===========新增配置===============");
 
         //查询该配置
         System.out.println("===========查询配置===============");
-        String configKey = ConfigTypeEnum.SITE_BASICCONFIG.getConfigKey();
         ConfigVo queryConfigVo = siteConfigService.selectConfigValueByConfigKey(configKey);
         System.out.println(queryConfigVo);
         System.out.println("===========查询配置===============");
 
         //更新配置
         System.out.println("===========更新配置===============");
-        json = "{\n" +
-                "        siteTitle: '仿生实验室1',  \n" +
-                "        teamTitle: '仿生实验室1',  \n" +
-                "        teamLogo: 'http://pictured-bedtest.oss-cn-beijing.aliyuncs.com/test/studio/8c44b79e-cd18-40ae-8d5a-5e2a0df303b7.png',\n" +
-                "        ISPN: '京公网安备11000002000001号', \n" +
-                "        siteCreateTime: '2017'\n" +
-                "      }";
-        configVo.setConfigValue(json);
+        configVo.setConfigValue(updateJson);
         configVo.setConfigId(queryConfigVo.getConfigId());
         System.out.println(siteConfigService.addOrUpdateSiteConfig(configVo));
         System.out.println("===========更新配置===============");
+
+        //查询该配置
+        System.out.println("===========查询配置===============");
+        queryConfigVo = siteConfigService.selectConfigValueByConfigKey(configKey);
+        System.out.println(queryConfigVo);
+        System.out.println("===========查询配置===============");
     }
 
 

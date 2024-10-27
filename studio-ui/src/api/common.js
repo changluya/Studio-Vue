b46f07e9a703334ee0231aa0a676c354c12eb688
changlu <939974883@qq.com>
2024-10-27 22:54:47 +0800

    [hotfix_1.3.x_1][studio-vue][去除studio-ui，使用studio-ui-vite替换，后台系统使用vite构建]

--- studio-ui/src/api/common.js
+++
@@ -1,6 +0,0 @@
-import { getToken } from '@/utils/auth'
-
-export default {
-  "uploadAction": process.env.BASE_URL + '/zf/api/upload',
-  "uploadFilesAction": process.env.BASE_URL + '/zf/api/upload/files'
-};
